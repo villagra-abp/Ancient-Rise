a80e6c384e968692116ea2d4009f5d3819148f49
AlexRoca95 <alexdragonit.drag@hotmail.com>
2017-12-05 12:46:31 +0100

    Base BehaviorTree

---
+++ Behavior.cpp
@@ -0,0 +1,45 @@
+#include "Behavior.h"
+
+
+Behavior::Behavior(): m_eStatus(BH_INVALID)
+{
+
+}
+
+
+Status Behavior::tick()
+{
+	if(m_eStatus==BH_RUNNING) // EJECUTANDOSE
+	{
+		onInitialize();
+	}
+
+	m_eStatus=update();
+
+	if(m_eStatus !=BH_RUNNING)
+	{
+		onTerminate(m_eStatus);
+	}
+
+	return m_eStatus; 
+}
+/*
+METODO QUE SE LLAMA SOLO UNA VEZ DESPUES DE QUE EL UPDATE TERMINE
+*/
+void onTerminate(Status)
+{
+	
+}
+
+/*
+METODO QUE SE LLAMA SOLO UNA VEZ DESPUES DE LLAMAR AL UPDATE 
+*/
+void onInitialize()
+{
+
+}
+
+Behavior::~Behavior()
+{
+    
+}
