40d9f2debc3aec7536b67bb31a85728a29ae66b9
Rubén Sánchez <barrikelo_ital@hotmail.com>
2018-02-21 18:42:31 +0100

    declaro matrices perspectiva y ortogonal

--- motorgrafico/sourcefiles/TCamara.cpp
+++ motorgrafico/sourcefiles/TCamara.cpp
@@ -7,14 +7,15 @@ TCamara::~TCamara(){
 
 }
 void setPerspectiva(float fovy, float aspect, float near, float far){
-	mProyeccion = glm::perspective(45.0f, 1024.0f / 768.0f, 0.1f, 200.0f);
+	glm::mat4 Proyeccion = glm::perspective(45.0f, 1024.0f / 768.0f, 0.1f, 200.0f);
 }
 void setParalela(float left, float right, float bottom, float top, float near, float far){
-	mProyeccion = glm::ortho(0.0f, 1024.0f,768.0ft,0.0f, 0.1f, 100.0f);
+	glm::mat4 Proyeccion = glm::ortho(0.0f, 1024.0f,768.0f,0.0f, 0.1f, 100.0f);
 }
 void TCamara::endDraw(){
 }
 void TCamara::beginDraw(){
 }
+
 
 
