0cbac78b026053f7f62581c8b12d70eb3712c5ef
Victor <vcv183@gmail.com>
2018-03-06 15:17:40 +0100

    Proyecciones TCamara

--- motorgrafico/sourcefiles/TCamara.cpp
+++ motorgrafico/sourcefiles/TCamara.cpp
@@ -6,11 +6,11 @@ TCamara::TCamara(){
 TCamara::~TCamara(){
 
 }
-void setPerspectiva(float fovy, float aspect, float near, float far){
-	glm::mat4 Proyeccion = glm::perspective(45.0f, 1024.0f / 768.0f, 0.1f, 200.0f);
+void TCamara::setPerspectiva(float fovy, float aspect, float near, float far){
+	mProyeccion = glm::perspective(fovy, aspect, near, far);
 }
-void setParalela(float left, float right, float bottom, float top, float near, float far){
-	glm::mat4 Proyeccion = glm::ortho(0.0f, 1024.0f,768.0f,0.0f, 0.1f, 100.0f);
+void TCamara::setParalela(float left, float right, float bottom, float top, float near, float far){
+	mProyeccion = glm::ortho(left, right, bottom, top, near, far);
 }
 void TCamara::endDraw(){
 }
