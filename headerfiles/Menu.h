3942edb71e1a1acef8e09d64e4018624624467ae
Rubén Sánchez <barrikelo_ital@hotmail.com>
2018-05-02 19:26:13 +0200

    Add files via upload

--- headerfiles/Menu.h
+++ headerfiles/Menu.h
@@ -11,10 +11,12 @@ class Menu : public Objeto
 	public:
 		Menu( Posicion* pos);
 		void* getNode();
+        void update(int n);
+        int getEstado();
 
 	private:
         void * menu;
-        
+        int estado=1;
 };
 
 #endif // MENU_H
