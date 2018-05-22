2fb723365d711e2823d19c53a5e8c0e4159d0cb8
AlexRoca95 <alexdragonit.drag@hotmail.com>
2018-05-22 16:56:27 +0200

    Animacion salto medio hecha

--- sourcefiles/Mundo.cpp
+++ sourcefiles/Mundo.cpp
@@ -1176,8 +1176,7 @@ void Mundo::cargaNivel()
             prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/saltoadelante/saltoadelante.txt", prota->getNode(),3));
             prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/correr/correr.txt", prota->getNode(),4));
             prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/saltocarrera/saltocarrera.txt", prota->getNode(),5));
-            prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/salto/salto.txt", prota->getNode(),6));
-            prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/reposo1/reposo1.txt", prota->getNode(),6));
+            prota->setNode(fachada->addAnimacion(0, 0, 10000, "resources/Animaciones/reposo1/reposo1.txt", prota->getNode(),7));
 
             break;
         }
